3336e3f45c272cbe6bdb445bc4bee375ba5306bb
bilykb <brettbilyk@gmail.com>
2021-08-17 22:47:32 +0000

    various syntax changes formed.  Comments added for functions. ASCII characters removed and replaced

--- client.js
+++ client.js
@@ -1,12 +1,16 @@
 const net = require('net');
-const { IP, PORT } = require('./constants')
+const { IP, PORT } = require('./constants');
 
+/**
+ * connect establishes a connection for the client to the snek server, and manages events
+ * @returns conn is returned to be used in other functions, such as input
+ */
 const connect = function() {
   const conn = net.createConnection({
     host: IP,
     port: PORT
   });
-  conn.setEncoding('utf8')
+  conn.setEncoding('utf8');
 
   conn.addListener('data', data => {
     console.log('Server says', data);
@@ -17,6 +21,6 @@ const connect = function() {
     conn.write("Name: BRB");
   });
   return conn;
-}
+};
 
 module.exports = { connect };
--- input.js
+++ input.js
@@ -1,6 +1,12 @@
 let connection;
 const { inputObject } = require('./constants');
 
+/**
+ * setupInput establishes how user input is managed
+ * @param {*} conn 
+ * @returns 
+ */
+
 const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;
@@ -9,18 +15,22 @@ const setupInput = function(conn) {
   stdin.on('data', handleUserInput);
   stdin.resume();
   return stdin;
-}
+};
 
+/**
+ * a function which binds various actions with keyboard characters
+ * @param {*} key 
+ */
 const handleUserInput = function(key) {
-    if (key === "\u0003") {
-      process.exit();
+  if (key === "\u0003") {
+    process.exit();
+  }
+  for (const [character, command] of Object.entries(inputObject)) {
+    // loop through input commands and send command to server
+    if (character === key) {
+      connection.write(command);
     }
-    for(const [unicode, command] of Object.entries(inputObject)) {
-      // loop through input commands and send command to server
-      if (unicode === key) {
-        connection.write(command);
-      }
-    }
+  }
 };
 
 module.exports = { setupInput };
