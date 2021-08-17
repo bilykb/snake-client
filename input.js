5492a26ff8ce0eef573aa61b2c0e45a1ac8d930d
bilykb <brettbilyk@gmail.com>
2021-08-17 20:34:08 +0000

    added wasd functionality as well as two canned messages to send to other clients

--- input.js
+++ input.js
@@ -1,17 +1,38 @@
-const handleUserInput = function() {
-  process.stdin.on('data', key => {
-    if (key === "\u0003") {
-      process.exit();
-    }
-  });
-};
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
+  stdin.on('data', handleUserInput);
   stdin.resume();
-  handleUserInput();
   return stdin;
 }
 
+const handleUserInput = function(key) {
+    if (key === "\u0003") {
+      process.exit();
+    }
+    if (key === "\u0077") {
+      connection.write("Move: up");
+    }
+    if (key === "\u0073") {
+      connection.write("Move: down");
+    }
+    if (key === "\u0061") {
+      connection.write("Move: left");
+    }
+    if (key === "\u0064") {
+      connection.write("Move: right");
+    }
+    if (key === "\u0031") {
+      connection.write("Say: Have a good day!");
+    }
+    if (key === "\u0032") {
+      connection.write("Say: I am so confused");
+    }
+};
+
 module.exports = { setupInput };
+
