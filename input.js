db277f0e6c84947d9d14ce032b3ba779e6ef52f2
bilykb <brettbilyk@gmail.com>
2021-08-17 00:14:56 +0000

    two functions that handle user input moved to input.js

---
+++ input.js
@@ -0,0 +1,17 @@
+const handleUserInput = function() {
+  process.stdin.on('data', key => {
+    if (key === "\u0003") {
+      process.exit();
+    }
+  });
+};
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput();
+  return stdin;
+}
+
+module.exports = { setupInput };
